1750dc28e7eaae6a394ad991e5e7c34fc5cabefe
Johan Sangalang <jnsangalang93@yahoo.com>
2024-06-12 22:45:48 +0000

    created BrowserRoute in main.tsx, and created some Routes within App.tsx (just the path to '/') , and created a Header.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,11 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './pages/Header';
 
 export default function App() {
-  const [serverData, setServerData] = useState('');
-
-  useEffect(() => {
-    async function readServerData() {
-      const resp = await fetch('/api/hello');
-      const data = await resp.json();
-
-      console.log('Data from server:', data);
-
-      setServerData(data.message);
-    }
-
-    readServerData();
-  }, []);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>{serverData}</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Header />}></Route>
+    </Routes>
   );
 }
